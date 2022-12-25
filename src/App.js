31ec4837398d893085d9ce4723dd04015bfd3770
Anamika4844 <anya.saxena@gmail.com>
2022-12-25 16:55:14 -0400

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Anamika Saxena and{" "}
+        <a
+          href="https://github.com/Anamika4844/weather-react-project"
+          target="_blank"
+        >
+          is a open sourced on Github{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
